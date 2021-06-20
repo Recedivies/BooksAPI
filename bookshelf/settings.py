16a393b0992a37ccb48ab16d59c8e7be18aae506
recedivies <ahmadhiprananta@gmail.com>
2021-06-20 21:50:25 +0700

    Final

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -102,7 +102,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
             'PORT': '5432',
         }
     }
-    
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
