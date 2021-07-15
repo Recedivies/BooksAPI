98d2be7d30897141b046c98ee012117643ebe52f
recedivies <ahmadhiprananta@gmail.com>
2021-07-15 14:07:19 +0700

    3rd commit

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
 import datetime
 import os
 from pathlib import Path
@@ -175,3 +176,5 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+
+django_heroku.settings(locals())
