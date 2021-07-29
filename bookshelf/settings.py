68ba0f57162ee7220fc6ce8969612b290a8aaf15
recedivies <ahmadhiprananta@gmail.com>
2021-07-29 22:18:56 +0700

    dev

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-6s+$6kwaa%eh91!jej_hts3n)&5(e$c3w4nreautaz&uf-2_l@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
